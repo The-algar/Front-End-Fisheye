03ca155c25ec083d0ca1b755c62747cc61efc24b
ERIC CLERMONTET <emc@go4.social>
2022-06-09 10:40:08 +0200

    Last Aria fixes

--- scripts/utils/ariaModals.js
+++ scripts/utils/ariaModals.js
@@ -6,7 +6,7 @@ function ariaCompliant(modal){
     
     //blocage du tab dans la modale
     document.addEventListener('keydown', (e) => {
-        if(e.code == 27) {
+        if(e.key == 27) {
             closeModal()
             closeCarousel()
         }
@@ -16,7 +16,7 @@ function ariaCompliant(modal){
     const firstFocusableElement = focusableContent[0];
     const lastFocusableElement = focusableContent[focusableContent.length-1];
     document.addEventListener('keydown', (e) => {
-        if (e.code !== 9){
+        if (e.key !== 9){
             return;
         } else if (e.shiftKey) { //pour un shift+tab
             if(document.activeElement == firstFocusableElement){
